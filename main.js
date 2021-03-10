653a3a3e6d2f040ec905f462bfb9acdd31e5e320
Dmytro Rusanovskyi <dmytrorysan@gmail.com>
2021-03-10 19:07:59 +0200

    all

---
+++ main.js
@@ -0,0 +1,5 @@
+// const { min } = require("./node_modules/math/min");
+// const { abs } = require("./node_modules/math/abs");
+// const { summ } = require("./node_modules/math/summ");
+const { min, abs, summ } = require("./node_modules/math/index");
+console.log(min([1, 2, 3, 4, 5]), abs(-99), summ([1, 2, 3, 4, 5]));
